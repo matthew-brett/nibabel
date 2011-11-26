63e3220ae2e216469d8803cb6c5aabee3bf6662c
Matthew Brett <matthew.brett@gmail.com>
2011-11-26 11:06:55 -0800

    NF+RF - expand draft of arraywriters
    Pull in some code from volumeutils; work out the interface

--- nibabel/arraywriters.py
+++ nibabel/arraywriters.py
@@ -12,16 +12,13 @@ They do have attributes:
 
 * array
 * out_dtype
-
-They may have attributes:
-
 * scale
 * inter
 
 They are designed to write arrays to a fileobj with reasonable memory
 efficiency.
 
-Subclasses of array writers may be able to scale the array or apply an
+Array writers may be able to scale the array or apply an
 intercept, or do something else to make sense of conversions between float and
 int, or between larger ints and smaller.
 """
@@ -33,7 +30,10 @@ class WriterError(Exception):
     pass
 
 
-class ArrayWriter(object):
+class ScaleInterArrayWriter(object):
+    # Working precision of scaling
+    scale_inter_type = np.float32
+
     def __init__(self, array, out_dtype=None):
         """ Initialize array writer
 
@@ -52,11 +52,27 @@ class ArrayWriter(object):
         """
         self._array = np.asanyarray(array)
         arr_dtype = self._array.dtype
+        self._scale = 1.0
+        self._inter = 0.0
         if out_dtype is None:
             out_dtype = arr_dtype
-        elif out_dtype not in (arr_dtype, arr_dtype.newbyteorder('S')):
-            raise ValueError('out_dtype needs to be same kind as array dtype')
+        else:
+            out_dtype = np.dtype(out_dtype)
         self._out_dtype = out_dtype
+        if np.can_cast(arr_dtype, out_dtype):
+            return
+        if 'V' in (arr_dtype.kind, out_dtype.kind):
+            raise WriterError('Cannot cast to or from non-numeric types')
+        if out_dtype.kind == 'c':
+            return
+        if arr_dtype.kind == 'c':
+            raise WriterError('Cannot cast complex types to non-complex')
+        if out_dtype.kind == 'f':
+            return
+        # Try to scale, raise error if we fail
+        self._scale_fi2i()
+        if np.all(np.isfinite([self.scale, self.inter])):
+            raise WriterError('Non-finite scaling or intercept')
 
     @property
     def array(self):
@@ -68,6 +84,88 @@ class ArrayWriter(object):
         """ Return `out_dtype` from arraywriter """
         return self._out_dtype
 
+    def _get_scale(self):
+        return self._scale
+    def _set_scale(self, val):
+        self._scale = self.scale_inter_type(val)
+    scale = property(_get_scale, _set_scale, None, 'get/set scale')
+
+    def _get_inter(self):
+        return self._inter
+    def _set_inter(self, val):
+        self._inter = self.scale_inter_type(val)
+    inter = property(_get_inter, _set_inter, None, 'get/set inter')
+
+    def _scale_fi2i(self):
+        """ Calculate / set scaling for floats/(u)ints to (u)ints
+        """
+        arr = self._array
+        arr_dtype = arr.dtype
+        out_dtype = self._out_dtype
+        assert out_dtype.kind in 'iu'
+        mn, mx = self.finite_range()
+        if mn == np.inf : # No valid data
+            return
+        if (mn, mx) == (0.0, 0.0): # Data all zero
+            return
+        if arr_dtype.kind == 'f':
+            # Float to (u)int scaling
+            self._scale_f2i()
+            return
+        # (u)int to (u)int
+        out_max, out_min = np.iinfo(out_dtype)
+        if mx <= out_max and mn >= out_min: # already in range
+            return
+        if out_min == 0 and mx < 0 and abs(mn) <= out_max: # sign flip?
+            # Careful: -1.0 * arr will be in scale_type precision
+            self.scale = -1.0
+            return
+        # (u)int to (u)int scaling
+        self._scale_i2i()
+
+    def _scale_f2i(self):
+        # with intercept
+        mn, mx = self.finite_range() # These will be floats
+        out_max, out_min = np.iinfo(self._out_dtype) # These integers
+        if out_min == 0: # uint
+            if mn < 0 and mx > 0:
+                # Maybe an intercept of the min will be enough
+                # NNB - overflow?
+                if (mx - mn) <= out_max:
+                    self.inter = mn
+                    return
+        # Overflow?
+        data_range = mx-mn
+        if data_range == 0:
+            self.inter = mn
+            return
+        type_range = out_max - out_min
+        self.scale = data_range / type_range
+        self.inter = mn - out_min * self.scale
+
+    def _scale_i2i(self):
+        # with intercept
+        mn, mx = self.finite_range() # These will be integers
+        out_max, out_min = np.iinfo(self._out_dtype) # These too
+        # Overflow?
+        data_range = mx-mn
+        if data_range == 0:
+            self.inter = mn
+            return
+        type_range = out_max - out_min
+        self.scale = data_range / type_range
+        self.inter = mn - out_min * self.scale
+
+    def finite_range(self):
+        """ Return (maybe cached) finite range of data array """
+        try:
+            return self._finite_range
+        except AttributeError:
+            pass
+        from .volumeutils import finite_range
+        self._finite_range = finite_range(self._array)
+        return self._finite_range
+
     def to_fileobj(self, fileobj, order='F'):
         """ Write array into `fileobj`
 
@@ -78,7 +176,8 @@ class ArrayWriter(object):
             order (Fortran or C) to which to write array
         """
         data = self._array
-        jbs = data.dtype == self._out_dtype.newbyteorder('S') # just byte swapped
+        out_dtype = self._out_dtype
+        scale, inter = self._scale, self._inter
         if not order in 'FC':
             raise ValueError('Order should be one of F or C')
         if order == 'F':
@@ -86,6 +185,54 @@ class ArrayWriter(object):
         if data.ndim < 2: # a little hack to allow 1D arrays in loop below
             data = [data]
         for dslice in data: # cycle over largest dimension to save memory
-            if jbs:
-                dslice = dslice.byteswap()
+            if inter != 0.0:
+                dslice = dslice - inter
+            if scale != 1.0:
+                dslice = dslice / scale
+            if dslice.dtype != out_dtype:
+                dslice = dslice.astype(out_dtype)
             fileobj.write(dslice.tostring())
+
+
+class ScaleArrayWriter(ScaleInterArrayWriter):
+    @property
+    def inter(self):
+        """ Intercept read only for slope writer """
+        return self._inter
+
+    def _scale_f2i(self):
+        # without intercept
+        mn, mx = self.finite_range() # These will be floats
+        out_max, out_min = np.iinfo(self._out_dtype) # These integers
+        if out_min == 0: # uint
+            if mn < 0 and mx > 0:
+                raise ValueError('Cannot scale negative and positive '
+                                'numbers to uint without intercept')
+            if mx < 0: # All input numbers < 0
+                self.scale = mn / out_max
+            else: # All input numbers > 0
+                self.scale = mx / out_max
+        else: # int
+            if abs(mx) >= abs(mn):
+                self.scale = mx / out_max
+            else: # Is this right?  Why?
+                self.scale = mn / out_min
+
+    def _scale_i2i(self):
+        # without intercept
+        mn, mx = self.finite_range() # These will be integers
+        out_max, out_min = np.iinfo(self._out_dtype) # These too
+        scale_type = self.scale_inter_type
+        if out_min == 0: # uint
+            if mn < 0 and mx > 0:
+                raise ValueError('Cannot scale negative and positive '
+                                'numbers to uint without intercept')
+            if mx < 0: # All input numbers < 0
+                self.scale = scale_type(mn) / out_max
+            else: # All input numbers > 0
+                self.scale = scale_type(mx) / out_max
+        else: # int
+            if abs(mx) >= abs(mn):
+                self.scale = scale_type(mx) / out_max
+            else: # Is this right?  Why?
+                self.scale = scale_type(mn) / out_min
--- nibabel/tests/test_arraywriters.py
+++ nibabel/tests/test_arraywriters.py
@@ -31,7 +31,8 @@ import numpy as np
 
 from ..py3k import BytesIO
 
-from ..arraywriters import ArrayWriter
+from ..arraywriters import ScaleInterArrayWriter, ScaleArrayWriter, WriterError
+
 from ..volumeutils import array_from_file
 
 from numpy.testing import (assert_array_almost_equal,
@@ -40,37 +41,77 @@ from numpy.testing import (assert_array_almost_equal,
 from nose.tools import assert_true, assert_equal, assert_raises
 
 
-def check_round_trip(writer, order='F'):
+FLOAT_TYPES = np.sctypes['float']
+COMPLEX_TYPES = np.sctypes['complex']
+INT_TYPES = np.sctypes['int']
+UINT_TYPES = np.sctypes['uint']
+CFLOAT_TYPES = FLOAT_TYPES + COMPLEX_TYPES
+IUINT_TYPES = INT_TYPES + UINT_TYPES
+NUMERIC_TYPES = CFLOAT_TYPES + IUINT_TYPES
+
+def round_trip(writer, order='F'):
     sio = BytesIO()
     arr = writer.array
     writer.to_fileobj(sio, order)
-    arr_back = array_from_file(arr.shape, writer.out_dtype, sio, order=order)
-    assert_array_equal(arr_back, arr)
-    return arr_back
+    return array_from_file(arr.shape, writer.out_dtype, sio, order=order)
 
 
-def test_arraywriter():
+def test_arraywriters():
     # Test initialize
     # Simple cases
-    arr = np.arange(10)
-    aw = ArrayWriter(arr)
-    assert_true(aw.array is arr)
-    assert_equal(aw.out_dtype, arr.dtype)
-    check_round_trip(aw)
-    # Byteswapped is OK
-    bs_arr = arr.byteswap().newbyteorder('S')
-    bs_aw = ArrayWriter(bs_arr)
-    check_round_trip(bs_aw)
-    bs_aw2 = ArrayWriter(bs_arr, arr.dtype)
-    check_round_trip(bs_aw2)
-    # Different type is not
-    assert_raises(ValueError, ArrayWriter, arr, np.float32)
-    # 2D array
-    arr2 = np.reshape(arr, (2, 5))
-    a2w = ArrayWriter(arr2)
-    arr_back = check_round_trip(a2w)
-    # Array comes back in written order
-    assert_true(arr_back.flags.f_contiguous)
-    # Or given order
-    arr_back = check_round_trip(a2w, 'C')
-    assert_true(arr_back.flags.c_contiguous)
+    for klass in (ScaleInterArrayWriter, ScaleArrayWriter):
+        for type in NUMERIC_TYPES:
+            arr = np.arange(10, dtype=type)
+            aw = klass(arr)
+            assert_true(aw.array is arr)
+            assert_equal(aw.out_dtype, arr.dtype)
+            assert_array_equal(arr, round_trip(aw))
+            # Byteswapped is OK
+            bs_arr = arr.byteswap().newbyteorder('S')
+            bs_aw = klass(bs_arr)
+            assert_array_equal(bs_arr, round_trip(bs_aw))
+            bs_aw2 = klass(bs_arr, arr.dtype)
+            assert_array_equal(bs_arr, round_trip(bs_aw2))
+            # 2D array
+            arr2 = np.reshape(arr, (2, 5))
+            a2w = klass(arr2)
+            # Default out - in order is Fortran
+            arr_back = round_trip(a2w)
+            assert_array_equal(arr2, arr_back)
+            arr_back = round_trip(a2w, 'F')
+            assert_array_equal(arr2, arr_back)
+            # C order works as well
+            arr_back = round_trip(a2w, 'C')
+            assert_array_equal(arr2, arr_back)
+            assert_true(arr_back.flags.c_contiguous)
+
+
+def test_to_float():
+    for in_type in NUMERIC_TYPES:
+        if in_type in IUINT_TYPES:
+            info = np.iinfo(in_type)
+            if info.min == 0: # uint
+                mn, mx, start, stop, step = info.min, info.max, 0, 100, 1
+            else: # int
+                mn, mx, start, stop, step = info.min, info.max, 0, 100, 1
+        else:
+            info = np.finfo(in_type)
+            mn, mx, start, stop, step = info.min, info.max, 0, 100, 0.5
+        arr = np.arange(start, stop, step, dtype=in_type)
+        arr[0] = mn
+        arr[-1] = mx
+        for out_type in CFLOAT_TYPES:
+            for klass in (ScaleInterArrayWriter, ScaleArrayWriter):
+                if in_type in COMPLEX_TYPES and out_type in FLOAT_TYPES:
+                    assert_raises(WriterError, klass, arr, out_type)
+                    continue
+                aw = klass(arr, out_type)
+                assert_true(aw.array is arr)
+                assert_equal(aw.out_dtype, out_type)
+                arr_back = round_trip(aw)
+                assert_array_equal(arr.astype(out_type), arr_back)
+                # Check too-big values overflowed correctly
+                out_min = np.finfo(out_type).min
+                out_max = np.finfo(out_type).max
+                assert_true(np.all(arr_back[arr > out_max] == np.inf))
+                assert_true(np.all(arr_back[arr < out_min] == -np.inf))
